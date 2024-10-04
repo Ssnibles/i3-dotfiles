cad4b5df0ae0ab0a2c850cf6fd2f33fbf082746a
Ssnibles <joshua.breite@gmail.com>
2024-10-04 15:54:50 +1300

    update - better plugin support for neovim

--- dot_config/nvim/lua/plugins/legendary.lua
+++ dot_config/nvim/lua/plugins/legendary.lua
@@ -1,9 +1,28 @@
 return {
   "mrjones2014/legendary.nvim",
-  -- since legendary.nvim handles all your keymaps/commands,
-  -- its recommended to load legendary.nvim before other plugins
   priority = 10000,
   lazy = false,
-  -- sqlite is only needed if you want to use frecency sorting
-  -- dependencies = { 'kkharji/sqlite.lua' }
+  dependencies = {
+    "kkharji/sqlite.lua",
+    "stevearc/dressing.nvim", -- optional, for better UI
+  },
+  opts = {
+    extensions = {
+      which_key = { auto_register = true },
+      lazy_nvim = { auto_register = true },
+    },
+    sort = {
+      -- sort most frequently used items to the top
+      frecency = true,
+      -- sort user-defined items before built-in items
+      user_items = true,
+    },
+    include_builtin = true,
+    include_legendary_cmds = true,
+    select_prompt = " Legendary ",
+    col_separator_char = "│",
+  },
+  keys = {
+    { "<leader>l", "<cmd>Legendary<cr>", desc = "Open Legendary" },
+  },
 }
