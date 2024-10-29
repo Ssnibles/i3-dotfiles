ed50d9a2cbbecba34d86aa24ed8d3e33cbebfd0b
Ssnibles <joshua.breite@gmail.com>
2024-10-29 23:02:16 +1300

    added line count script and alias

--- dot_config/fish/aliases/scripts_aliases.fish
+++ dot_config/fish/aliases/scripts_aliases.fish
@@ -1,3 +1,4 @@
 # Usefull scripts
-alias re-add="~/repos/scripts/chezmoi_add.sh"
+alias re-add="~/Documents/scripts/chezmoi_add.sh"
 alias re-shell="source ~/.config/fish/config.fish"
+alias line-count="~/Documents/scripts/line_count.sh"
