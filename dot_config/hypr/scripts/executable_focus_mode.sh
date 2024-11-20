ae183772ac061e22055b0fa25af8e57173ed55b8
Ssnibles <joshua.breite@gmail.com>
2024-11-20 13:09:28 +1300

    general update

--- dot_config/hypr/scripts/executable_focus_mode.sh
+++ dot_config/hypr/scripts/executable_focus_mode.sh
@@ -3,22 +3,31 @@
 # File to store the current state
 STATE_FILE="$HOME/.config/hypr/gaps_state"
 
-# Function to apply gaps
+# Function to apply gaps and reduce opacity
 apply_gaps() {
-    # Change the bellow gaps_out and gaps_in to your liking
-    hyprctl --batch "keyword general:gaps_out 15;keyword general:gaps_in 10;keyword decoration:rounding 8"
-    echo "1" > "$STATE_FILE"
+  hyprctl --batch "\
+    keyword general:gaps_out 15;\
+    keyword general:gaps_in 10;\
+    keyword decoration:rounding 8;\
+    keyword decoration:active_opacity 0.85;\
+    keyword decoration:inactive_opacity 0.85"
+  echo "1" >"$STATE_FILE"
 }
 
-# Function to remove gaps
+# Function to remove gaps and set full opacity
 remove_gaps() {
-    hyprctl --batch "keyword general:gaps_out 0;keyword general:gaps_in 0;keyword decoration:rounding 0"
-    echo "0" > "$STATE_FILE"
+  hyprctl --batch "\
+    keyword general:gaps_out 0;\
+    keyword general:gaps_in 0;\
+    keyword decoration:rounding 0;\
+    keyword decoration:active_opacity 1.0;\
+    keyword decoration:inactive_opacity 1.0"
+  echo "0" >"$STATE_FILE"
 }
 
 # Check if the state file exists, if not create it
 if [ ! -f "$STATE_FILE" ]; then
-    echo "0" > "$STATE_FILE"
+  echo "0" >"$STATE_FILE"
 fi
 
 # Read the current state
@@ -26,7 +35,7 @@ current_state=$(cat "$STATE_FILE")
 
 # Toggle the state
 if [ "$current_state" = "0" ]; then
-    apply_gaps
+  apply_gaps
 else
-    remove_gaps
+  remove_gaps
 fi
