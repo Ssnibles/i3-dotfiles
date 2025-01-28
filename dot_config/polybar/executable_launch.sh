ea70f236715850a9061ae914b18a96471a1b38a5
Ssnibles <joshua.breite@gmail.com>
2025-01-28 21:52:45 +1300

    update: too much to say

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -1,16 +1,24 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 0.5; done
 
-# for multimonitor
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example & disown
-  done
-else
-  polybar --reload example & disown
-fi
+# Function to launch polybar
+launch_polybar() {
+  MONITOR=$1 polybar &
+}
+
+# Get all connected monitors
+MONITORS=$(xrandr --query | grep " connected" | cut -d" " -f1)
+
+# Launch polybar on each monitor
+echo "Launching Polybar on monitors: $MONITORS"
+echo "$MONITORS" | while read -r monitor; do
+  echo "Launching on $monitor"
+  launch_polybar "$monitor"
+done
+
+echo "Polybar launched..."
